224c6b3fe33de00e1f128a995a97f530593c0d3c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-08 10:19:11 +0330

    chore: import router from api.api_v1.api as podcast_router

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
+from api.api_v1.api import router as podcast_router
+
+
 app = FastAPI()
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=True)
