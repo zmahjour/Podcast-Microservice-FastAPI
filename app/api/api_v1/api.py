bbac0c6247780159442bdbc78f561ae0d1812b48
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 16:53:47 +0330

    chore: import router from .endpoints.interactions as interactions_router

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .endpoints.podcasts import router as podcasts_router
+from .endpoints.interactions import router as interactions_router
 
 
 router = APIRouter()
 router.include_router(podcasts_router)
+router.include_router(interactions_router)
