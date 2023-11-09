82691fbc553e38851d903f7fbc551a8b0ffe094c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 14:16:11 +0330

    chore: define Settings class

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,15 @@
 from pydantic_settings import BaseSettings
 
 
+class Settings(BaseSettings):
+    # MongoDB
+    MONGODB_HOST: str
+    MONGODB_PORT: int
+
+    RSS_APP_BASE_URL: str
+
+    class Config:
+        env_file = (
+            "/home/samane/Documents/MaktabSharif/FinalProject/Project/Podcast/.env"
+        )
+
