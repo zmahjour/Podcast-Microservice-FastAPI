1285d0c5bf9f03324c9894d77b49c7203bfb7289
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 01:56:23 +0330

    chore: import asynccontextmanager, aioredis and settings

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
+import aioredis
 from api.api_v1.api import router as podcast_router
+from core.config import settings
+
 
 
 app = FastAPI()
