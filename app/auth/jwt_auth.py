cc34fde2acf226a5d7ec6d6c330b455d389417cc
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 01:55:20 +0330

    chore: define JWTAuthentication class

--- app/auth/jwt_auth.py
+++ app/auth/jwt_auth.py
@@ -3,3 +3,51 @@ import jwt
 from core.config import settings
 
 
+class JWTAuthentication:
+    async def authenticate(self, request, auth_header):
+        try:
+            token = self.get_token_from_header(header=auth_header)
+        except:
+            return None
+
+        try:
+            payload = self.decode_jwt_token(token=token)
+        except jwt.exceptions.InvalidSignatureError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid signature."
+            )
+        except jwt.exceptions.ExpiredSignatureError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Access token expired."
+            )
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+        jti = payload.get("jti")
+        redis = request.app.state.redis
+
+        if not await redis.get(jti):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token."
+            )
+
+        return payload
+
+    @staticmethod
+    async def delete_jti_from_cache(request, jti):
+        redis = request.app.state.redis
+        await redis.delete(jti)
+
+    @staticmethod
+    def get_token_from_header(header):
+        token = header.replace("Bearer", "").replace(" ", "")
+        return token
+
+    @staticmethod
+    def decode_jwt_token(token):
+        payload = jwt.decode(
+            token, key=settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        return payload
+
+
