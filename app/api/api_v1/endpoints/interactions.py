35831c692a975bf53243c5a95eebbc14096809a3
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 01:59:11 +0330

    chore: import required classes

--- app/api/api_v1/endpoints/interactions.py
+++ app/api/api_v1/endpoints/interactions.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Request, Header
 from fastapi.responses import JSONResponse
 import httpx
-
+from db.database import likes_collection
+from auth.jwt_auth import auth
+from core.config import settings
 
 router = APIRouter(prefix="/interactions", tags=["interactions"])
 
