e570b05f5296e52391aee2d7433cd46d89015d1e
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 14:14:53 +0330

    chore: import reuqired modules and classes

--- app/api/api_v1/endpoints/podcasts.py
+++ app/api/api_v1/endpoints/podcasts.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
+from fastapi.responses import JSONResponse
+import httpx
+import json
+from core.config import settings
 
 
 router = APIRouter(prefix="/podcasts", tags=["podcasts"])
