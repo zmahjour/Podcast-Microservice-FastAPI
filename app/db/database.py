5372ce72a0750ff674816d9536c08db69f833c8c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 01:58:43 +0330

    chore: define mongodb configs

--- app/db/database.py
+++ app/db/database.py
@@ -1,3 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import settings
 
+# MongoDB
+mongodb_url = f"mongodb://{settings.MONGODB_HOST}:{settings.MONGODB_PORT}"
+client = AsyncIOMotorClient(mongodb_url)
+database = client.rss
+likes_collection = database.get_collection("likes")
